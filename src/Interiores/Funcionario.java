5194a8503c3c8829879b1dc6da7bb7985286efab
AldoNicolas <74311295+AldoNicolas@users.noreply.github.com>
2020-11-15 23:27:43 -0500

    Create Funcionario.java
    creacion de clase e inicializacion de metodos

---
+++ src/Interiores/Funcionario.java
@@ -0,0 +1,29 @@
+
+package proyecto1p2t_2020_g1;
+
+public class Funcionario {
+    public void iniciarMenuOpFun(){
+    }
+    public void registrarNuevoAnimal(){
+        
+    }
+    public void consultarAnimales(){
+        
+    }
+    public void registrarInteresadoAdopcion(){
+        
+    }
+    public void registrarAdopcion(){
+        
+    }
+    public void consultaAdopciones(){
+        
+    }
+    public void consultarRegistrados(){
+        
+    }
+    public void cerrrarSesion(){
+        
+    }
+    
+}
