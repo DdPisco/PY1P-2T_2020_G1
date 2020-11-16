8b097858eb6fe8344c99f71bf57775ededb2b31b
AldoNicolas <74311295+AldoNicolas@users.noreply.github.com>
2020-11-15 23:26:58 -0500

    Create Administrador.java
    creacion de clase e inicializacion de metodos

---
+++ src/Interiores/Administrador.java
@@ -0,0 +1,35 @@
+
+package proyecto1p2t_2020_g1;
+
+public class Administrador {
+    private int cuentaBancaria;
+    
+    public void iniciarMenuOpAdmin(){
+        
+    }
+    public void registarEmpleados(){
+        
+    }
+    public void consultaVeterinaria(){
+        
+    }
+    public void registrosVeterinaria(){
+        
+    }
+    public void registrosGastosVeterinaria(){
+        
+    }
+    public double calcularPresupuestosMensual(){
+        return 0;
+        
+    }
+    public void enviarCorreosAdopciones(){
+        
+    }
+    public void cerrrarSesion(){
+        
+    }
+   
+    
+}
+    
