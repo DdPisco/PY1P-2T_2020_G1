9209de4dce9835c64dedec2bdd3092dd520efa24
DdPisco <74310186+DdPisco@users.noreply.github.com>
2020-11-15 23:19:46 -0500

    Update Animal.java

--- src/Interiores/Animal.java
+++ src/Interiores/Animal.java
@@ -10,5 +10,13 @@ package Interiores;
  * @author Dustin Pisco
  */
 public class Animal {
-    
+    private Date fechaIngreso;
+    private String nombre;
+    private String raza;
+    private char sexo;
+    private double peso;
+    private int edad;
+    private int cod;
+    private String observaciones;
+    private boolean adoptado;
 }
