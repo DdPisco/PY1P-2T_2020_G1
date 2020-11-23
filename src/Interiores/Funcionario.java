598cf259b01904c9d5ad34629f0a2effafd3aade
nmmoran <74307794+nmmoran@users.noreply.github.com>
2020-11-22 19:52:49 -0500

    cracion de menu para usuario funcionario

--- src/Interiores/Funcionario.java
+++ src/Interiores/Funcionario.java
@@ -1,9 +1,58 @@
+package Interiores;
 
-package proyecto1p2t_2020_g1;
+import Interiores.Usuario;
+import java.util.Scanner;
 
-public class Funcionario {
+/**
+ *
+ * @author nicol
+ */
+public class Funcionario extends Usuario{
+    public Scanner sc;
     public void iniciarMenuOpFun(){
+        sc=new Scanner(System.in);
+        String op = "";
+    System.out.println("Bienvenido");
+    do {
+      System.out.println("1. Registrar Nuevo Animal ");
+      System.out.println("2. Consultar Animales en la Fundación ");
+      System.out.println("3. Registrar Interesado Adopción ");
+      System.out.println("4. Registrar Adopción ");
+      System.out.println("5. Consultar Adopciones ");
+      System.out.println("6. Consultar Adoptantes");
+      System.out.println("7. Cerrar Sesion");
+      System.out.println("Ingrese opcion");
+      op = sc.nextLine();
+
+      switch (op) {
+      case "1":
+        registrarNuevoAnimal();
+        break;
+      case "2":
+        consultarAnimales();
+        break;
+      case "3":
+        registrarInteresadoAdopcion();
+        break;
+      case "4":
+        registrarAdopcion();  
+        break;
+      case "5":
+        consultaAdopciones();
+        break;
+      case "6":
+        consultarRegistrados();
+        break;
+      case "7":
+        cerrarSesion();
+        break;
+      default:
+        System.out.println("Opcion invalida");
+        break;
+      }
+    } while (!op.equals("7"));
     }
+    
     public void registrarNuevoAnimal(){
         
     }
@@ -22,8 +71,7 @@ public class Funcionario {
     public void consultarRegistrados(){
         
     }
-    public void cerrrarSesion(){
+    public void cerrarSesion(){
         
     }
-    
 }
