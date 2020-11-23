9c29e692f10a106f44d3dd58eb41aea093fafb73
nmmoran <74307794+nmmoran@users.noreply.github.com>
2020-11-22 19:51:42 -0500

    agregacion de menu para usuario administrador

--- src/Interiores/Administrador.java
+++ src/Interiores/Administrador.java
@@ -1,11 +1,54 @@
+package Interiores;
 
-package proyecto1p2t_2020_g1;
+import Interiores.Usuario;
+import java.util.Scanner;
 
-public class Administrador {
+/**
+ *
+ * @author nicol
+ */
+public class Administrador extends Usuario{
     private int cuentaBancaria;
-    
+    public Scanner sc;
     public void iniciarMenuOpAdmin(){
-        
+        sc=new Scanner(System.in);
+        String op = "";
+    System.out.println("Bienvenido");
+    do {
+      System.out.println("1. Registrar Empleado");
+      System.out.println("2. Consultar y Registrar Veterinarias");
+      System.out.println("3. Consultar y Registrar Gasto Veterinaria ");
+      System.out.println("4. Calcular Presupuesto Mensual ");
+      System.out.println("5. Enviar Correo a Interesados ");
+      System.out.println("6. Cerrar Sesion");
+      System.out.println("Ingrese opcion");
+      op = sc.nextLine();
+
+      switch (op) {
+      case "1":
+        registarEmpleados();
+        break;
+      case "2":
+        consultaVeterinaria();
+        registrosVeterinaria();
+        break;
+      case "3":
+        registrosGastosVeterinaria();
+        break;
+      case "4":
+        calcularPresupuestosMensual();  
+        break;
+      case "5":
+        enviarCorreosAdopciones();
+        break;
+      case "6":
+        System.out.println("Adios");
+        break;  
+      default:
+        System.out.println("Opcion invalida");
+        break;
+      }
+    } while (!op.equals("6"));
     }
     public void registarEmpleados(){
         
@@ -30,6 +73,11 @@ public class Administrador {
         
     }
    
-    
+    public void setCuentaBancaria(int cuentaBancaria){
+        this.cuentaBancaria=cuentaBancaria;       
+    }
+    public int getCuentaBancaria(){
+        return this.cuentaBancaria;
+    }
+
 }
-    
