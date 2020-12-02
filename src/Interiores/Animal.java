ed3e82a830534f7fe32e424ae12750cc186f1001
nicol <nicol@Nicole>
2020-12-02 05:36:30 -0500

    Desarrollo de la clase Animal

--- src/Interiores/Animal.java
+++ src/Interiores/Animal.java
@@ -11,7 +11,7 @@ import java.util.Date;
  *
  * @author Dustin Pisco
  */
-public class Animal {
+public abstract class Animal {
     private Date fechaIngreso;
     private String nombre;
     private String raza;
@@ -21,4 +21,96 @@ public class Animal {
     private int cod;
     private String observaciones;
     private boolean adoptado;
+
+    public Animal() {
+    }
+
+    
+    public Animal(Date fechaIngreso, String nombre, String raza, char sexo, double peso, int edad, int cod, String observaciones) {
+        this.fechaIngreso = fechaIngreso;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.edad = edad;
+        this.cod = cod;
+        this.observaciones = observaciones;
+        this.adoptado=false;
+    }
+
+    public  abstract double calcularCosto();
+    
+    
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(Date fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public boolean isAdoptado() {
+        return adoptado;
+    }
+
+    public void setAdoptado(boolean adoptado) {
+        this.adoptado = adoptado;
+    }
+    
 }
