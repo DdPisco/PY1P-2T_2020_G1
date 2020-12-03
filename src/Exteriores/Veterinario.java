95cc1359714935f215d83e82a2dd5b37849f8e27
AldoNicolas <74311295+AldoNicolas@users.noreply.github.com>
2020-12-03 03:32:24 -0500

    modificacion de clases cliente, gastosveterinario, veterinario

--- src/Exteriores/Veterinario.java
+++ src/Exteriores/Veterinario.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Exteriores;
 
 /**
  *
- * @author Dustin Pisco
+ * @author Aldo Mendoza
  */
 public class Veterinario {
+    private String nombre;
+    private String numero;
+    private String correo;
+
+    public Veterinario(String nombre, String numero, String correo) {
+        this.nombre = nombre;
+        this.numero = numero;
+        this.correo = correo;
+    }
+    
+    
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+    
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
     
 }
--- src/Interiores/Cliente.java
+++ src/Interiores/Cliente.java
@@ -1,16 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Interiores;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Dustin Pisco
  */
 public class Cliente {
-    public Cliente(){
-        
+    private String nombre;
+    private String cedula;
+    private String telefono;
+    private String direcction;
+    private ArrayList<String> intereses;
+
+    public Cliente(String nombre, String cedula, String telefono, String direcction, ArrayList<String> intereses) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.direcction = direcction;
+        this.intereses = intereses;
     }
+    
+    //GETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getDirecction() {
+        return direcction;
+    }
+
+    public ArrayList<String> getIntereses() {
+        return intereses;
+    }
+    
+    // SETTERS
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDirecction(String direcction) {
+        this.direcction = direcction;
+    }
+
+    public void setIntereses(ArrayList<String> intereses) {
+        this.intereses = intereses;
+    }
+    
+    
+    
+    
+    
 }
+
