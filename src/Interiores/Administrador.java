6522762afaf18c93b62ff26aeb25c9f4f9872ca3
AldoNicolas <74311295+AldoNicolas@users.noreply.github.com>
2020-12-04 09:30:39 -0500

    Update Administrador.java
    Modificacion de metodos de clase

--- src/Interiores/Administrador.java
+++ src/Interiores/Administrador.java
@@ -1,12 +1,15 @@
 package Interiores;
 
+import Exteriores.Veterinario;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 /**
  *
- * @author nicol
+ * @author Aldo
  */
 public class Administrador extends Usuario{
     private int cuentaBancaria;
@@ -56,6 +59,24 @@ public class Administrador extends Usuario{
           }
         } while (!op.equals("6"));
     }
+    /**
+     * Permite convertir un String en fecha (Date).
+     * @param fecha Cadena de fecha dd/MM/yyyy
+     * @return Objeto Date
+     */
+    public static Date ParseFecha(String fecha)
+    {
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        Date fechaDate = null;
+        try {
+            fechaDate = formato.parse(fecha);
+        } 
+        catch (ParseException ex) 
+        {
+            System.out.println(ex);
+        }
+        return fechaDate;
+    }
     
     public void registarEmpleados(){
        System.out.println("Ingreso de nuevo empleado");
@@ -72,10 +93,11 @@ public class Administrador extends Usuario{
        String correo = sc.nextLine();
        
        System.out.println("Ingrese fecha de inicio del empleado");
-       String fecha = sc.nextLine();
+       String fecha1 = sc.nextLine();
        
        System.out.println("Ingrese Sueldo del empleado");
        double suel = sc.nextDouble();
+       sc.nextLine();
        
        System.out.println("Ingrese Usuario del empleado");
        String user = sc.nextLine();
@@ -84,36 +106,98 @@ public class Administrador extends Usuario{
        String contra = sc.nextLine();
        
        Usuario us1 = new Usuario(user, contra);
+              
+       Date fecha = Administrador.ParseFecha(fecha1);
        
-       
+       Fundacion.getListEmpleados().add(new Empleado(nom,direc,telf,correo,fecha,suel));
        
        for (Usuario us : Fundacion.getListUsuarios()){
-           if(Fundacion.getListUsuarios().contains(us)){
-               Fundacion.getListUsuarios().add(us1);
-           }else{
+           if(Fundacion.getListUsuarios().contains(us1)){
                System.out.println("el usuario ya existe");
+           }else{
+               Fundacion.getListUsuarios().add(us1);
            }
        }
       
     
     }
-    public void consultaVeterinaria(){
-        
+    public ArrayList consultaVeterinaria(){
+        System.out.println("Consulta de Veterinarias");
+        return Fundacion.veterinarias();
     }
     public void registrosVeterinaria(){
+        System.out.println("Registro de veterinarias");
         
+        System.out.println("Ingrese el nombrede la veterinaria");
+        String nombVet = sc.nextLine();
+        
+        System.out.println("Ingrese el numero de la veterinaria");
+        String telfVet = sc.nextLine();
+        
+        System.out.println("Ingrese el correo de la veterinaria");
+        String correoVet = sc.nextLine();
+        
+        Veterinario vet1 = new Veterinario(nombVet,telfVet,correoVet);
+        
+        for (Veterinario vet : Fundacion.veterinarias()){
+           if(Fundacion.getListUsuarios().contains(vet1)){
+               System.out.println("el usuario ya existe");
+           }else{
+               Fundacion.veterinarias().add(vet1);
+           }
+       }
     }
     public void registrosGastosVeterinaria(){
         
-    }
-    public double calcularPresupuestosMensual(){
-        return 0;
         
     }
+    public double calcularPresupuestosMensual(){
+        
+        return 0;
+        
+     
+    }
+    private double CalculodeEmpleados(){
+        double totsueldo_empleados=0;
+        double TotalAdmin = 0;
+        for (Empleado e : Fundacion.getListEmpleados()){
+            double suel= e.getSueldo();
+            totsueldo_empleados += suel;
+    
+        }return TotalAdmin = totsueldo_empleados+150.0;
+  
+    }
+    
+    private double CalculodeAnimales(){
+        
+        for (Animal a : Fundacion.getAnimales()){
+            double TotalAnimales;
+            double TotalPerros = 0;
+            double TotalGatos = 0;
+            TotalAnimales = TotalGatos + TotalPerros;
+            if (a instanceof Perro ){
+                Perro p = (Perro)a;
+                double costosPerro;
+                costosPerro = p.calcularCosto();
+                TotalPerros+=costosPerro;
+                return TotalPerros;
+                
+            }else if (a instanceof Gato){
+                Gato g = (Gato)a;
+                double costosGato;
+                costosGato = a.calcularCosto();
+                TotalGatos+=costosGato;
+                return TotalGatos;    
+            }
+        
+        return TotalAnimales = TotalPerros + TotalGatos;
+        }return -1;
+    }
+    
     public void enviarCorreosAdopciones(){
         
     }
-    public void cerrrarSesion(){
+    public void cerrrarSesion(){    
         
     }
    
