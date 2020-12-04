22eab7919cc3ed9167cfc021eaf95e011b82717e
DdPisco <74310186+DdPisco@users.noreply.github.com>
2020-12-04 03:30:31 -0500

    Update Cliente.java

--- src/Interiores/Cliente.java
+++ src/Interiores/Cliente.java
@@ -8,68 +8,66 @@ import java.util.ArrayList;
  * @author Dustin Pisco
  */
 public class Cliente {
-    private String nombre;
+    private String nombre; 
     private String cedula;
-    private String telefono;
-    private String direcction;
+    private String telf;
+    private String direccion;
+    private String correo;
     private ArrayList<String> intereses;
-    
     public Cliente(){
+        
     }
 
-    public Cliente(String nombre, String cedula, String telefono, String direcction, ArrayList<String> intereses) {
-        this.nombre = nombre;
-        this.cedula = cedula;
-        this.telefono = telefono;
-        this.direcction = direcction;
-        this.intereses = intereses;
-    }
-    
-    //GETTERS
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getCedula() {
         return cedula;
     }
 
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public String getDirecction() {
-        return direcction;
-    }
-
-    public ArrayList<String> getIntereses() {
-        return intereses;
-    }
-    
-    // SETTERS
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public void setCedula(String cedula) {
         this.cedula = cedula;
     }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public String getTelf() {
+        return telf;
     }
 
-    public void setDirecction(String direcction) {
-        this.direcction = direcction;
+    public void setTelf(String telf) {
+        this.telf = telf;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public ArrayList<String> getIntereses() {
+        return intereses;
     }
 
     public void setIntereses(ArrayList<String> intereses) {
         this.intereses = intereses;
     }
     
-    
-    
-    
-    
+    public String toString(){
+        return cedula+"\t"+nombre+"\t"+correo+"\t"+direccion;
+    }
+   
 }
-
