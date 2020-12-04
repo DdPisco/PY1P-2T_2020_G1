86bfc7016379e364cd6ecaf0a96eee3076ecff41
AldoNicolas <74311295+AldoNicolas@users.noreply.github.com>
2020-12-04 08:20:44 -0500

    Update Perro.java
    @Override metodo calcular costo

--- src/Interiores/Perro.java
+++ src/Interiores/Perro.java
@@ -1,33 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Interiores;
-
+import java.util.Scanner;
 /**
  *
- * @author nicol
+ * @author Aldo , Nicole
  */
 public class Perro extends Animal {
     private Tamanio tamanio;
-    
+    Scanner sc  = new Scanner(System.in);
+
+    public Perro() {
+        
+    }
     
     @Override
     public double calcularCosto(){
-       return 2.50; 
+        double COSTO_POR_KILO_COMIDA;
+        double KILOS_COMIDA_POR_MES;
+        double GASTO_MENSUAL;
+        System.out.println("Ingrese el tamaño del perro: ");
+        String tamanio = sc.nextLine();
+        System.out.println("Ingrese la cantidad de perros paseados: ");
+        double COSTOPASEOS =sc.nextDouble();
+        sc.nextLine();
+        
+        switch (tamanio.toUpperCase()){
+            case "GRANDE":
+                COSTO_POR_KILO_COMIDA = 4;
+                KILOS_COMIDA_POR_MES = 10;
+                GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES + COSTOPASEOS;
+                return GASTO_MENSUAL;
+            
+            case "MEDIANO":
+                COSTO_POR_KILO_COMIDA = 5;
+                KILOS_COMIDA_POR_MES = 6;
+                GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES + COSTOPASEOS;
+                return GASTO_MENSUAL;
+                
+            case "PEQUEÑO":
+                COSTO_POR_KILO_COMIDA = 5;
+                KILOS_COMIDA_POR_MES = 4;
+                GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES + COSTOPASEOS;
+                return GASTO_MENSUAL;
+    
+        }
+        return -1;
     }
+     
+    
+    
     public Tamanio getTamanio() {
         return tamanio;
     }
 
-    public void setTamanio(Tamanio tamanio) {
+    public  void setTamanio(Tamanio tamanio) {
         this.tamanio = tamanio;
     }
     
     @Override
     public String toString(){
         return getFechaIngreso()+"  "+getNombre()+"  "+getRaza()+"  "+getSexo()+"  "+ getPeso()
-                +"  "+getEdad()+"  "+getTamanio()+ "  "+getObservaciones();
+                +"\n"+getEdad()+"  "+getTamanio()+ "  "+getObservaciones();
     }
 }
