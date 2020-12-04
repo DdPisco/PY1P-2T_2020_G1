1ca4f64d9a97f2fea64db6e9c64c338193316c6f
AldoNicolas <74311295+AldoNicolas@users.noreply.github.com>
2020-12-04 08:21:44 -0500

    Update Gato.java
    @Override del metodo

--- src/Interiores/Gato.java
+++ src/Interiores/Gato.java
@@ -1,19 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Interiores;
 
 /**
  *
- * @author nicol
+ * @author Aldo, Nicole
  */
 public class Gato extends Animal {
+    
+            
     @Override
     public double calcularCosto(){
-       return 2.50; 
+        //GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES
+        int  edad = super.getEdad();
+        double GASTO_MENSUAL;
+        double COSTO_POR_KILO_COMIDA;
+        double KILOS_COMIDA_POR_MES;
+        if (edad < 8 ){
+            COSTO_POR_KILO_COMIDA = 3;
+            KILOS_COMIDA_POR_MES = 2.5;           
+            return GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES;
+        }else {
+            COSTO_POR_KILO_COMIDA = 5;
+            KILOS_COMIDA_POR_MES = 2.5;           
+            return GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES; 
+        
+        }
+              
     }
+    
     public String toString(){
         return getFechaIngreso()+"  "+getNombre()+"  "+getRaza()+"  "+getSexo()+"  "+ getPeso()
                 +"  "+getEdad()+"  "+"\t"+ "  "+getObservaciones();
