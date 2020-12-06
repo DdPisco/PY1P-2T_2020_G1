e4aa082aa9b04af11b9da449e22f049e321e79a3
nmmoran <74307794+nmmoran@users.noreply.github.com>
2020-12-06 00:19:08 -0500

    actualizacion clase gato

--- src/Interiores/Gato.java
+++ src/Interiores/Gato.java
@@ -1,33 +1,28 @@
-
 package Interiores;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 /**
  *
- * @author Aldo, Nicole
+ * @author nicol
  */
 public class Gato extends Animal {
-    
-            
-    @Override
-    public double calcularCosto(){
-        //GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES
-        int  edad = super.getEdad();
-        double GASTO_MENSUAL;
-        double COSTO_POR_KILO_COMIDA;
-        double KILOS_COMIDA_POR_MES;
-        if (edad < 8 ){
-            COSTO_POR_KILO_COMIDA = 3;
-            KILOS_COMIDA_POR_MES = 2.5;           
-            return GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES;
-        }else {
-            COSTO_POR_KILO_COMIDA = 5;
-            KILOS_COMIDA_POR_MES = 2.5;           
-            return GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES; 
-        
-        }
-              
+    public Gato(LocalDate fechaIngreso, String nombre, String raza, String sexo, double peso, int edad, String observaciones){
+        super(fechaIngreso,nombre,raza,sexo,peso,edad,observaciones);
     }
     
+    @Override
+    public double calcularCosto(){
+        int edad = getEdad();
+        double c;
+        if(edad<8){
+            c=3*2.5;
+            return c;
+        }
+        c = 5*2.5;
+        return c;
+    }
     public String toString(){
         return getFechaIngreso()+"  "+getNombre()+"  "+getRaza()+"  "+getSexo()+"  "+ getPeso()
                 +"  "+getEdad()+"  "+"\t"+ "  "+getObservaciones();
