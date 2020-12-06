d91b9bd9cd261a46223d1f64f08df6f87659371b
nmmoran <74307794+nmmoran@users.noreply.github.com>
2020-12-06 00:17:27 -0500

    correcion clase perro

--- src/Interiores/Perro.java
+++ src/Interiores/Perro.java
@@ -1,64 +1,48 @@
 package Interiores;
-import java.util.Scanner;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 /**
  *
- * @author Aldo , Nicole
+ * @author nicol
  */
 public class Perro extends Animal {
     private Tamanio tamanio;
-    Scanner sc  = new Scanner(System.in);
-
-    public Perro() {
-        
+    
+    public Perro(LocalDate fechaIngreso, String nombre, String raza, String sexo, double peso, int edad, String observaciones,Tamanio tamnio){
+        super(fechaIngreso,nombre,raza,sexo,peso,edad,observaciones);
+        this.tamanio=tamanio;
     }
     
     @Override
     public double calcularCosto(){
-        double COSTO_POR_KILO_COMIDA;
-        double KILOS_COMIDA_POR_MES;
-        double GASTO_MENSUAL;
-        System.out.println("Ingrese el tamaño del perro: ");
-        String tamanio = sc.nextLine();
-        System.out.println("Ingrese la cantidad de perros paseados: ");
-        double COSTOPASEOS =sc.nextDouble();
-        sc.nextLine();
-        
-        switch (tamanio.toUpperCase()){
-            case "GRANDE":
-                COSTO_POR_KILO_COMIDA = 4;
-                KILOS_COMIDA_POR_MES = 10;
-                GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES + COSTOPASEOS;
-                return GASTO_MENSUAL;
-            
-            case "MEDIANO":
-                COSTO_POR_KILO_COMIDA = 5;
-                KILOS_COMIDA_POR_MES = 6;
-                GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES + COSTOPASEOS;
-                return GASTO_MENSUAL;
-                
-            case "PEQUEÑO":
-                COSTO_POR_KILO_COMIDA = 5;
-                KILOS_COMIDA_POR_MES = 4;
-                GASTO_MENSUAL = COSTO_POR_KILO_COMIDA*KILOS_COMIDA_POR_MES + COSTOPASEOS;
-                return GASTO_MENSUAL;
-    
+        double c = 0;
+        switch(tamanio){
+            case GRANDE:
+                c = 4*10+1;
+                break;
+            case MEDIANO:
+                c = 5*6+1;
+                break;
+            case PEQUENO:
+                c = 5*4+1;
+                break;
+            default:
+                return -1;
         }
-        return -1;
+        return c;  
     }
-     
-    
-    
     public Tamanio getTamanio() {
         return tamanio;
     }
 
-    public  void setTamanio(Tamanio tamanio) {
+    public void setTamanio(Tamanio tamanio) {
         this.tamanio = tamanio;
     }
     
     @Override
     public String toString(){
-        return getFechaIngreso()+"  "+getNombre()+"  "+getRaza()+"  "+getSexo()+"  "+ getPeso()
-                +"\n"+getEdad()+"  "+getTamanio()+ "  "+getObservaciones();
+        return getFechaIngreso()+"    "+getNombre()+"    "+getRaza()+"    "+getSexo()+"    "+ getPeso()+"    "+getEdad()+"    "+getTamanio()+ "    "+getObservaciones();
     }
 }
