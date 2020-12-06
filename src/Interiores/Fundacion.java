f47aa8092bafac569e9e452ff1ab34d5fd1c3d28
nmmoran <74307794+nmmoran@users.noreply.github.com>
2020-12-06 00:21:04 -0500

    correcion clase fundacion

--- src/Interiores/Fundacion.java
+++ src/Interiores/Fundacion.java
@@ -7,72 +7,33 @@ package Interiores;
 
 import Exteriores.GastoVeterinaria;
 import Exteriores.Veterinario;
+import Interfaces.UI;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
  * @author nicol
  */
 public class Fundacion {
-    private static ArrayList<Usuario> listUsuarios;
-    private static  ArrayList<Animal> animales;
-    private static  ArrayList<Veterinario> veterinarias;
-    private static  ArrayList<GastoVeterinaria> gastosVeterinaria;
-    private static  ArrayList<Cliente> clientes;
-    private static ArrayList<Adopcion> adopciones;
-
+    public ArrayList<Usuario> listUsuarios = new ArrayList<>();
+    public ArrayList<Animal> listAnimales = new ArrayList<>();
+    public ArrayList<Veterinario> veterinarias = new ArrayList<>();
+    public ArrayList<GastoVeterinaria> gastosVeterinaria = new ArrayList<>();
+    public  ArrayList<Cliente> clientes = new ArrayList<>();
+    public ArrayList<Adopcion> adopciones = new ArrayList<>();
+    public ArrayList<String> lIntereses = new ArrayList<>();
+    
+    //Animal a1 = new Gato(UI.parseFecha("12/11/2019"), "siri", "siames", "macho", 15.00, 5,"n/a");
+    
+    
+   
     public Fundacion() {
-        
-    }
-    
-    public static ArrayList<Usuario> getListUsuarios() {
-        return listUsuarios;
     }
 
-    public void setListUsuarios(ArrayList<Usuario> listUsuarios) {
-        this.listUsuarios = listUsuarios;
-    }
-
-    public static ArrayList<Animal> getAnimales() {
-        return animales;
-    }
-
-    public static void setAnimales(ArrayList<Animal> animales) {
-        Fundacion.animales = animales;
-    }
-    
-    public static ArrayList<Veterinario> veterinarias() {
-        return veterinarias;
-    }
-
-    public static void setVeterinarias(ArrayList<Veterinario> veterinarias) {
-        Fundacion.veterinarias = veterinarias;
-    }
-
-    public static ArrayList<GastoVeterinaria> getGastosVeterinaria() {
-        return gastosVeterinaria;
-    }
-
-    public static void setGastosVeterinaria(ArrayList<GastoVeterinaria> gastosVeterinaria) {
-        Fundacion.gastosVeterinaria = gastosVeterinaria;
-    }
-
-    public static ArrayList<Cliente> getClientes() {
-        return clientes;
-    }
-
-    public static void setClientes(ArrayList<Cliente> clientes) {
-        Fundacion.clientes = clientes;
-    }
-
-    public static ArrayList<Adopcion> getAdopciones() {
-        return adopciones;
-    }
-
-    public static void setAdopciones(ArrayList<Adopcion> adopciones) {
-        Fundacion.adopciones = adopciones;
-    }
+   
    
    
     
 }
+
